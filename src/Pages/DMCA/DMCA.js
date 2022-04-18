ee20aefa7f74c94052cebe690396d32917bd202d
Isaiah Cedano <isaiahcedano6@gmail.com>
2022-04-18 13:05:41 -0500

    added dmca page

--- src/Pages/DMCA/DMCA.js
+++ src/Pages/DMCA/DMCA.js
@@ -1,6 +1,44 @@
 const DMCA = () => {
   return (
-    <></>
+    <>
+      <div className={"container dmca-container"}>
+        <p>
+          None of the files shown here are hosted or transmitted by this server. The links are provided solely by this site’s users. The administrator of this site (coursehome.netlify.app) cannot be held responsible for what its users post, or any other actions of its users. You may not use this site to distribute or download any material when you do not have the legal rights to do so. It is your own responsibility to adhere to these terms.
+        </p>
+
+        <p>
+          coursehome.netlify.app is in compliance with 17 U.S.C. § 512 and the Digital Millennium Copyright Act (“DMCA”). It is our policy to respond to any infringement notices and take appropriate actions under the Digital Millennium Copyright Act (“DMCA”) and other applicable intellectual property laws. If your copyrighted material has been posted on coursehome.netlify.app or if links to your copyrighted material are returned through our search engine and you want this material removed, you must provide a written communication that details the information listed in the following section. Please be aware that you will be liable for damages (including costs and attorneys’ fees) if you misrepresent information listed on our site that is infringing on your copyrights. We suggest that you first contact an attorney for legal assistance on this matter.
+        </p>
+
+        <p>
+          The following elements must be included in your copyright infringement claim:
+        </p>
+
+        <ul>
+          <li>
+            <p>
+              • Provide evidence of the authorized person to act on behalf of the owner of an exclusive right that is allegedly infringed.
+            </p>
+          </li>
+          <li><p>• Provide sufficient contact information so that we may contact you. You must also include a valid email address.</p></li>
+          <li><p>• You must identify in sufficient detail the copyrighted work claimed to have been infringed and including at least one search term under which the material appears in coursehome.netlify.app’s search results.</p></li>
+          <li><p>• A statement that the complaining party has a good faith belief that use of the material in the manner complained of is not authorized by the copyright owner, its agent, or the law.</p></li>
+          <li><p>• Must be signed by the authorized person to act on behalf of the owner of an exclusive right that is allegedly being infringed.</p></li>
+        </ul>
+
+        <p>
+          The Website is designed to provide its visitors with links to other websites that may host files. coursehome.netlify.app does not host any files nor links directly to files.
+        </p>
+
+        <p>
+          coursehome.netlify.app will remove any link per trademark or copyright owner’s request. Send full URL of the link along with your company’s email address to coursehome22@gmail.com. All requests will be processed within 1-2 business days. Here is a link to a sample DMCA Notice for your convenience: http://www.futurequest.net/Services/TOS/DMCA/DMCANotice.php
+        </p>
+
+        <p>
+          Note that emailing your complaint to other parties such as our Internet Service Provider will not expedite your request and may result in a delayed response due the complaint not properly being filed.
+        </p>
+      </div>
+    </>
   );
 };
 
