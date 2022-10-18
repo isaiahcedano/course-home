f9ed5e44d491a54a94ba42200f5c5381331ae921
Isaiah Cedano <95046643+isaiahcedano@users.noreply.github.com>
2022-10-18 17:14:50 -0500

    Update Home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,7 +6,7 @@ const Home = ({products}) => {
 
   return (
     <>
-      <MidHomeSection text={"You have an opportunity, take it."}/>
+      <MidHomeSection text={"Request a Course: +51983523557. You have an opportunity, take it."}/>
       {
 
         Object.entries(products).length ?
